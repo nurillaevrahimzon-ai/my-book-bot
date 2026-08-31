c10699b545a907af2be07ec331a112cc2482ea73
nurillaevrahimzon-ai <nurillaevrahimzon@gmail.com>
2026-08-31 21:51:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,26 @@
+import os
+from threading import Thread
+from flask import Flask
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+  return 'OK'
+
+
+def run():
+  port = int(os.environ.get('PORT', 8080))
+  app.run(host='0.0.0.0', port=port)
+
+
+def keep_alive():
+  Thread(target=run).start()
+
+
+keep_alive()
+
 import telebot
 from telebot import types
 
